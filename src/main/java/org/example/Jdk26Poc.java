087cd0b9dd291294730837aeb79a1df50212040d
Andrei Lima <andrei.lima75@gmail.com>
2026-07-09 20:51:46 -0300

    jdk poc

--- src/main/java/org/example/Jdk26Poc.java
+++ src/main/java/org/example/Jdk26Poc.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 public class Jdk26Poc {
 
     static void main() throws Exception {
 
         primitivePatterns();
+        http3Client();
 
     }
 
@@ -24,4 +31,24 @@ public class Jdk26Poc {
         System.out.println();
     }
 
+    static void http3Client() throws Exception {
+        System.out.println("2. HTTP/3 HttpClient:");
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://httpbin.org/get"))
+                    .timeout(Duration.ofSeconds(5))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("  HTTP Response: " + response.statusCode() + " (HTTP/3 capable in JDK 26)");
+        } catch (Exception e) {
+            System.out.println("  HTTP demo (network may vary): " + e.getMessage().substring(0, 50));
+        }
+        System.out.println();
+    }
 }
